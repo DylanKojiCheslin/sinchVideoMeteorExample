c221a805e9b93b75db9c480472a57bb53ad3dd29
dylan <dylan.kojicheslin@gmail.com>
2016-12-31 11:55:02 -0700

    template outline

---
+++ client/templates/videoChat.js
@@ -0,0 +1,18 @@
+import SinchClient from 'sinch-rtc';
+
+Template.videoChat.helpers({
+
+});
+
+Template.name.events({
+  "click #start-chat": function(event, template){
+
+  },
+  "click #stop-chat": function(event, template){
+
+  }
+});
+
+Template.VideoChat.onRendered({
+
+});
