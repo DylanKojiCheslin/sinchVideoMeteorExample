31ddbc22a36053666fbf0e0a87f4e5900ccf2719
dylan <dylan.kojicheslin@gmail.com>
2017-01-02 11:48:29 -0700

    templates

--- client/templates/videoChat.js
+++ client/templates/videoChat.js
@@ -1,18 +1,34 @@
 import SinchClient from 'sinch-rtc';
 
+Template.videoChat.onRendered({
+
+  // const template = this;
+  //
+  // sinchClient = new SinchClient({
+  //   applicationKey: Meteor.settings.public.SINCH_KEY,
+  //   capabilities: {calling: true, video: true},
+  //   supportActiveConnection: true,
+  //   onLogMessage: function(message) {
+  //       console.log(message.message);
+  //   },
+  // });
+});
+
 Template.videoChat.helpers({
 
 });
 
 Template.name.events({
-  "click #start-chat": function(event, template){
+
+  "click #anwser-chat": function(event, template){
+
+  },
+  "submit #start-chat": function(event, template){
+    // event.preventDefault();
+    // let roomName = event.target.room-name.value;
 
   },
   "click #stop-chat": function(event, template){
 
   }
-});
-
-Template.VideoChat.onRendered({
-
 });
